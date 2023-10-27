a5feb0c77a14ee0ac26774e72e5fca76449a9bcd
piszosta <piotrszostak@gmail.com>
2023-10-27 15:41:36 +0200

    kontaktsjemaInProgress

--- force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
+++ force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
@@ -1,7 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
 import kontaktsjemaBilde from '@salesforce/resourceUrl/KontaktskjemaLogo';
 import index from '@salesforce/resourceUrl/index';
 
 export default class Kontaktskjema extends LightningElement {
     bildeKontaktskjema = kontaktsjemaBilde;
+
+    @track breadcrumbs = [
+        {
+            label: 'nav.no',
+            href: ''
+        },
+        {
+            label: 'Arbeidsgiver',
+            href: 'mine-samtaler'
+        },
+        {
+            label: 'Kontakt NAV',
+            href: 'detail'
+        },
+        {
+            label: 'Kontaktskjema',
+            href: 'detail'
+        }
+    ];
+
+    renderedCallback() {
+        loadStyle(this, index);
+    }
 }
