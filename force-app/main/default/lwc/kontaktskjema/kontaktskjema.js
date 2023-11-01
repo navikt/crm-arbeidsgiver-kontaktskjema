3b9ef4eabcbd9143a76a1fdd2bfa7035e30a81bc
piszosta <piotrszostak@gmail.com>
2023-11-01 17:47:17 +0100

    added shared component Input, errorText, deleted textFieldForm

--- force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
+++ force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import kontaktsjemaBilde from '@salesforce/resourceUrl/KontaktskjemaLogo';
 import index from '@salesforce/resourceUrl/index';
@@ -28,4 +28,22 @@ export default class Kontaktskjema extends LightningElement {
     renderedCallback() {
         loadStyle(this, index);
     }
+
+    handleResize() {
+        const img = this.template.querySelector('[data-id="imageBanner"]');
+        if (window.innerWidth < 800) {
+            img.style.display = 'none';
+        } else {
+            img.style.display = 'flex';
+        }
+    }
+
+    connectedCallback() {
+        window.addEventListener('resize', this.handleResize.bind(this));
+    }
+
+    disconnectedCallback() {
+        window.removeEventListener('resize', this.handleResize.bind(this));
+    }
+
 }
