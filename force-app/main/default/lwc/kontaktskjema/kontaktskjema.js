e9292bcc25f1f1d46ece675422a25c7793cf92df
Andre Colle <56640917+raac09@users.noreply.github.com>
2023-11-07 12:35:12 +0100

    Radio buttons

--- force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
+++ force-app/main/default/lwc/kontaktskjema/kontaktskjema.js
@@ -6,6 +6,20 @@ import index from '@salesforce/resourceUrl/index';
 export default class Kontaktskjema extends LightningElement {
     bildeKontaktskjema = kontaktsjemaBilde;
 
+    @track selectedTheme = '';
+    @track selectedContactedEmployeeRep = '';
+    @track checkedRekruttere = false;
+    @track checkedForebygge = false;
+    @track checkedYesOrNo = false;
+
+    @track fieldValues = { 
+        FullName: '',
+        OrganizationNumber: '',
+        Email: '',
+        Phone: ''
+
+    }
+
     @track breadcrumbs = [
         {
             label: 'nav.no',
@@ -25,6 +39,43 @@ export default class Kontaktskjema extends LightningElement {
         }
     ];
 
+    themeOptions = [
+        { label: 'Rekruttere og inkludere', value: 'Rekruttere og inkludere', name: 'theme', checked: false},
+        { label: 'Forebygge sykefravær', value: 'Forebygge sykefravær', name: 'theme', checked: false}
+    ];
+
+    contactedEmployeeRepOptions = [
+        { label: 'Ja', value: 'Ja', name: 'contactedEmpRep', checked: false },
+        { label: 'Nei', value: 'Nei', name: 'contactedEmpRep', checked: false }
+    ];
+
+    handleTheme(event) {
+        console.log('theme test: ');
+        this.selectedTheme = event.detail;
+
+        if (this.selectedTheme[0].checked === true) {
+            this.checkedRekruttere = true;
+            this.checkedForebygge = false;
+        } else if (this.selectedTheme[1].checked === true) {
+            this.checkedRekruttere = false;
+            this.checkedForebygge = true;
+        } else {
+            this.checkedRekruttere = false;
+            this.checkedForebygge = false;
+        }
+    }
+
+    handleContactedEmployeeRep(event) {
+        console.log('yes or no test: ');
+        this.selectedContactedEmployeeRep = event.detail;
+
+        if (selectedContactedEmployeeRep[0].checked === true) {
+            this.checkedYesOrNo = true;
+        } else {
+            this.checkedYesOrNo = false;
+        }
+    }
+
     renderedCallback() {
         loadStyle(this, index);
     }
@@ -41,6 +92,8 @@ export default class Kontaktskjema extends LightningElement {
     connectedCallback() {
         window.addEventListener('resize', this.handleResize.bind(this));
     }
+
+  
 
     disconnectedCallback() {
         window.removeEventListener('resize', this.handleResize.bind(this));
